72f2e69559a75cf0ff123aabd5aead8f3aea307d
AnatolyPanushkin <anatolypanushkin@mail.ru>
2024-05-10 15:54:45 +0300

    Adding of new random friend after report

--- SocialNetwork/Domain/Events/ReportUser/ReportUser.cs
+++ SocialNetwork/Domain/Events/ReportUser/ReportUser.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SocialNetwork.Application.DTOs;
 using SocialNetwork.Application.Services.UserServices;
+using SocialNetwork.Domain.Aggregates;
+using SocialNetwork.Domain.Common;
+using SocialNetwork.Infrastructure.Data;
 
 namespace SocialNetwork.Domain.Events.ReportUser;
 
@@ -11,13 +15,33 @@ public class ReportUser
 
 public record ReportUserCommand(ReportUserDto ReportUserDto) : IRequest;
 
-public class ReportUserCommandHandler(IUserService userService):IRequestHandler<ReportUserCommand>
+public class ReportUserCommandHandler(IUserService userService, SocialNetworkContext context):IRequestHandler<ReportUserCommand>
 {
+    private readonly SocialNetworkContext _context = context;
     private readonly IUserService _userService = userService;
-    
+
     public async Task Handle(ReportUserCommand request, CancellationToken cancellationToken)
     {
         await _userService.ReportUser(request.ReportUserDto);
-        //Добавить поиск нового рандомного друга
+        
+        var randomFriendsOfUser = await _context.RandomFriends.Where(randFriends => randFriends.User == Guid.Parse(request.ReportUserDto.UserId))
+            .Select(randFriends => randFriends.RandomFriendOfUser).ToListAsync(cancellationToken: cancellationToken);
+
+        var randomUsersList = _context.Users.Where(u => u.Id != Guid.Parse(request.ReportUserDto.UserId) && !randomFriendsOfUser.Contains(u.Id))
+            .ToList();
+
+        if (!randomUsersList.Any())
+        {
+            throw new AvailableRandomFriendsNotFound();
+        }
+
+        var randomUser = randomUsersList[new Random().Next(randomUsersList.Count())];
+                
+        var newRandomFriend = new RandomFriend(request.ReportUserDto.UserId, randomUser.Id.ToString());
+        var newFriendForRandom = new RandomFriend(randomUser.Id.ToString(),request.ReportUserDto.UserId);
+        await _context.RandomFriends.AddAsync(newRandomFriend);
+        await _context.RandomFriends.AddAsync(newFriendForRandom);
+        await _context.SaveChangesAsync();
     }
 }
+    
